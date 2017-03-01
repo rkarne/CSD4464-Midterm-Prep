9183ed0c158192213068a7c85343db3b4a91f911
Roja Jayashree <c0689497@mylambton.ca>
2017-03-01 02:57:24 -0500

    final commit with all changes

--- src/java/midtermprep/Guestbook.java
+++ src/java/midtermprep/Guestbook.java
@@ -6,6 +6,7 @@
 package midtermprep;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -23,9 +24,15 @@ import javax.inject.Named;
 @Named
 @ApplicationScoped
 public class Guestbook {
-    private List<GuestbookPost> guestbookPosts = new ArrayList<>();
+    private  List<GuestbookPost> guestbookPosts;
+    private GuestbookPost gs;
+
+    public GuestbookPost getGs() {
+        return gs;
+    }
 
     public Guestbook() {
+        gs = new GuestbookPost(-1, "", "");
         refresh();
     }
 
@@ -35,6 +42,7 @@ public class Guestbook {
     
     private void refresh() {
         try {
+            guestbookPosts = new ArrayList<>();
             guestbookPosts.clear();
             Connection conn = Database.getConnection();
             Statement stmt = conn.createStatement();
@@ -51,5 +59,58 @@ public class Guestbook {
             Logger.getLogger(Guestbook.class.getName()).log(Level.SEVERE, null, ex);
             guestbookPosts.clear();
         }
+    }
+    
+    public String addPost(){
+         gs = new GuestbookPost(-1, "", "");
+        return "addPost";
+    }
+    
+    public String saveMessage(){
+         try (Connection conn = Database.getConnection()) {
+            if (gs.getId() >= 0) {
+                String sql = "UPDATE guestbook SET author = ?, message = ? WHERE id = ?";
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                pstmt.setString(1, gs.getAuthor());
+                pstmt.setString(2, gs.getMessage());
+                pstmt.setInt(3, gs.getId());
+                pstmt.executeUpdate();
+            } else {
+                String sql = "INSERT INTO guestbook ( author, message) VALUES (?,?)";
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                //pstmt.setInt(1, user.getId());
+                pstmt.setString(1, gs.getAuthor());
+                pstmt.setString(2, gs.getMessage());
+                pstmt.executeUpdate();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Guestbook.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        refresh();
+        return "displaybook";
+    }
+    
+    public String view(GuestbookPost book){
+        gs = book;
+        return "displaybook";
+    }
+    
+    public String editdetails(){
+        return "addPost";
+    }
+            
+    public String delete(){
+        try (Connection conn = Database.getConnection()) {
+            if (gs.getId() >= 0) {
+                String sql = "DELETE FROM guestbook WHERE id = ?";
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                pstmt.setInt(1, gs.getId());
+                pstmt.executeUpdate();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Guestbook.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        refresh();  
+        return "index";
     }
 }
